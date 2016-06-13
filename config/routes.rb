5331cfcfe874ee7ce47e5cef8c31e249449b8bac
Matt <matthewmorgante@gmail.com>
2016-06-13 11:52:58 +0200

    listings page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'listings#index'
   get 'pages/about'
   get 'pages/contact'
+  # adds new URL at localhost/seller
+  get 'seller' => "listings#seller"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
